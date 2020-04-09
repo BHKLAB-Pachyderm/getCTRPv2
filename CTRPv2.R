b7f1141e0963cb72eb6f118becbcf4ae1d6c4623
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-04-09 15:35:54 -0400

    Update CTRPv2.R

--- CTRPv2.R
+++ CTRPv2.R
@@ -215,7 +215,7 @@ rownames(drug_all) <- drug_all[ , "unique.drugid"]
 
 drug_all <- drug_all[rownames(ctrp.drugs),]
 ctrp.drugs[,c("smiles","inchikey","cid","FDA")] <- drug_all[,c("smiles","inchikey","cid","FDA")]
-    
+colnames(ctrp.drugs)[which(names(ctrp.drugs) == "drugid")] <- "unique.drugid"  
 
 standardizeRawDataConcRange <- function(sens.info, sens.raw){
 	unq.drugs <- unique(sens.info$drugid)
@@ -308,27 +308,29 @@ standardizeRawDataConcRange <- function(sens.info, sens.raw){
 }
 		 
 #add cellosaurus disease type to cell-info
-disease <- cell_all$Cellosaurus.Disease.Type[match(ctrp.cells$cellid, cell_all$unique.cellid)]
+		 
+colnames(ctrp.cells)[which(names(ctrp.cells) == "cellid")] <- "unique.cellid"
+disease <- cell_all$Cellosaurus.Disease.Type[match(ctrp.cells$unique.cellid, cell_all$unique.cellid)]
 ctrp.cells$Cellosaurus.Disease.Type <- disease		 
 	
 #add cellosaurus assession to cell-info
-assession <- cell_all$Cellosaurus.Accession.id[match(ctrp.cells$cellid, cell_all$unique.cellid)]
+assession <- cell_all$Cellosaurus.Accession.id[match(ctrp.cells$unique.cellid, cell_all$unique.cellid)]
 ctrp.cells$Cellosaurus.Accession.id <- assession
 		 
 #add pharmacodb id to cell-info
-pdb <- cell_all$PharmacoDB.id[match(ctrp.cells$cellid, cell_all$unique.cellid)]
+pdb <- cell_all$PharmacoDB.id[match(ctrp.cells$unique.cellid, cell_all$unique.cellid)]
 ctrp.cells$PharmacoDB.id <- pdb
 
 #add study tissue id to cell_info
-study_tissue <- cell_all$unique.tissueid.fromstudies[match(ctrp.cells$cellid, cell_all$unique.cellid)]
+study_tissue <- cell_all$unique.tissueid.fromstudies[match(ctrp.cells$unique.cellid, cell_all$unique.cellid)]
 ctrp.cells$unique.tissueid.fromstudies <- study_tissue
 		 
 #add study cell-line type to cell_info
-cell_type <- cell_all$CellLine.Type[match(ctrp.cells$cellid, cell_all$unique.cellid)]
+cell_type <- cell_all$CellLine.Type[match(ctrp.cells$unique.cellid, cell_all$unique.cellid)]
 ctrp.cells$CellLine.Type <- cell_type
 		 
 #add metastatic info to cell_info		 
-metastatic <- cell_all$Metastatic[match(ctrp.cells$cellid, cell_all$unique.cellid)]
+metastatic <- cell_all$Metastatic[match(ctrp.cells$unique.cellid, cell_all$unique.cellid)]
 ctrp.cells$Metastatic <- metastatic		 
 		 
 standardize <- standardizeRawDataConcRange(sens.info = sensitivityInfo, sens.raw = raw.sensitivity)
